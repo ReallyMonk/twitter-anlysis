8abc1a2a8d8e3af74811d4dcdb629208df88650b
ReallyMonkey <hourunlinxa@gmail.com>
2021-11-04 17:45:50 +0800

    'new'

--- distribution_visual.py
+++ distribution_visual.py
@@ -26,7 +26,11 @@ def visualize(polarity, subjectivity, category, cat_num):
     plotpath = './plot/'
 
     # build dataframe structure
-    ps_df = pd.DataFrame({'polarity': polarity, 'subjectivity': subjectivity, 'category': category})
+    ps_df = pd.DataFrame({
+        'polarity': polarity,
+        'subjectivity': subjectivity,
+        'category': category
+    })
 
     if False:
         # polarity distribution plot
@@ -51,7 +55,10 @@ def visualize(polarity, subjectivity, category, cat_num):
     # seperate dataframe and sort according to amount
     # get top k cat and num
     top_k_num = {}
-    top_k_cat_num = ps_df.groupby('category', as_index=False).count().sort_values(by='polarity', ascending=False).head(cat_num)
+    top_k_cat_num = ps_df.groupby('category',
+                                  as_index=False).count().sort_values(
+                                      by='polarity',
+                                      ascending=False).head(cat_num)
     for cnt_num in top_k_cat_num.iterrows():
         cnt_num = cnt_num[1]
         #print(cnt_num['category'], cnt_num['polarity'])
@@ -60,7 +67,8 @@ def visualize(polarity, subjectivity, category, cat_num):
     return
 
     # seperate dataframe and sort according to amount
-    top_k_cat = ps_df.groupby('category', as_index=False).count().sort_values(by='polarity', ascending=False)['category'].head(cat_num)
+    top_k_cat = ps_df.groupby('category', as_index=False).count().sort_values(
+        by='polarity', ascending=False)['category'].head(cat_num)
     top_k_list = list(top_k_cat)
     print(top_k_cat)
     print(top_k_list)
@@ -70,7 +78,8 @@ def visualize(polarity, subjectivity, category, cat_num):
     labels = []
     graphs = []
 
-    spt_df = ps_df.loc[ps_df['category'].isin(top_k_list)]  #.groupby('category')
+    spt_df = ps_df.loc[ps_df['category'].isin(
+        top_k_list)]  #.groupby('category')
     # reorder spt_df with topkcat list
     spt_df['category'] = spt_df['category'].astype('category')
     spt_df['category'].cat.reorder_categories(top_k_list, inplace=True)
@@ -88,7 +97,9 @@ def visualize(polarity, subjectivity, category, cat_num):
         sing_cate_data = sing_cate[1]
         print(type(sing_cate))
 
-        g = plt.scatter(sing_cate_data['polarity'], sing_cate_data['subjectivity'], c=cmap(i))
+        g = plt.scatter(sing_cate_data['polarity'],
+                        sing_cate_data['subjectivity'],
+                        c=cmap(i))
         labels.append(sing_cate[0])
         graphs.append(g)
     '''plt.scatter(ps_df['polarity'], ps_df['subjectivity'], marker='o')
@@ -107,19 +118,43 @@ data_path = './mentions.csv'
 
 def plot_network(data_path):
     # read data and data processing
-    data = pd.read_csv(data_path, index_col=[0])
-    nodes = data.columns.values.tolist()
-
     cluster_num = 112
+    data = pd.read_csv(data_path, index_col=[0])
 
-    print(data.sum(axis=1).sort_values(ascending=False)[:20])
+    # build degree_matrix
+    degrees = data.sum(axis=0).sort_values(ascending=False)
+    print(degrees.items())
+    nodes = [index for index, _ in degrees.items()]
+    all_zeros = np.zeros((len(degrees), len(degrees)))
+    degree_matrix = pd.DataFrame(all_zeros, columns=nodes, index=nodes)
+    for index, value in degrees.items():
+        degree_matrix[index][index] = value
+    print(degree_matrix)
+
+    # build adjcency matrix
+    adj_matrix = data.copy(deep=True)
+    for index in nodes:
+        adj_matrix[index][index] = 0
+    adj_matrix = adj_matrix + adj_matrix.T
+    print(adj_matrix)
+
+    # build laplacian matrix and indicator matrix
+    lap_matrix = degree_matrix - adj_matrix
+    ax_degree_matrix = degree_matrix.copy()
+    for index in nodes:
+        ax_degree_matrix[index][index] = ax_degree_matrix[index][index]**(-0.5)
+    print(ax_degree_matrix)
+    std_lap_matrix = ax_degree_matrix * lap_matrix * ax_degree_matrix
+    print(std_lap_matrix)
+
+    #print(data.sum(axis=1).sort_values(ascending=False)[:20])
 
     #nodes = nodes[0:50]
     # print(nodes)
     cmap = plt.get_cmap('Spectral', cluster_num)
     # cmap_clr = cmap._segmentdata
-    for i in range(cmap.N):
-        print(matplotlib.colors.rgb2hex(cmap(i)))
+    #for i in range(cmap.N):
+    #    print(matplotlib.colors.rgb2hex(cmap(i)))
     # build node list
     #graph = nx.random_geometric_graph(112, 0.125)
     graph = nx.Graph()
@@ -129,10 +164,19 @@ def plot_network(data_path):
     for source in nodes:
         for target in nodes:
             if data[source][target] > 0 and source != target:
-                graph.add_edge(source, target, weight=data[source][target])
+                graph.add_edge(source,
+                               target,
+                               weight=data[source][target],
+                               colors='#666666')
 
-    nx.spring_layout(graph, scale=2)
-    nx.draw(graph, with_labels=False, font_weight='bold', node_size=100, alpha=0.7)
+    options = {"edge_color": "#666666"}
+    nx.shell_layout(graph, scale=2)
+    nx.draw(graph,
+            with_labels=True,
+            font_size=10,
+            node_size=50,
+            alpha=0.7,
+            **options)
     plt.show()
 
 
